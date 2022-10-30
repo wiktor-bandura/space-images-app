0953c2c6a8d9755f9e5bbca3e269ccb11ad3b153
wiktor-bandura <band.wiktor@gmail.com>
2022-10-30 14:57:04 +0100

    Changed app styling and made code more readable

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import './index.scss';
 import 'babel-polyfill';
 import getData from './js/get-data';
 import { container, showPhotos } from './js/show-photos';
+const main = document.querySelector('.main');
 const form = document.querySelector('form');
-const searchForm = document.querySelector('.search-form');
 const noResults = document.querySelector('.no-results');
 
 form.addEventListener('submit', e => {
@@ -14,7 +14,8 @@ form.addEventListener('submit', e => {
     getData(searchFraze)
         .then(data => {
             if(data.items.length === 0) {
-                searchForm.classList.contains('got-items') && searchForm.classList.remove('got-items');
+                main.classList.contains('got-items') &&
+                main.classList.remove('got-items');
                 noResults.innerHTML = `We didn't found images you want.`;
                 setTimeout(() => noResults.innerHTML = '', 3000);
             } else {
@@ -23,7 +24,7 @@ form.addEventListener('submit', e => {
                     showPhotos(href)
                         .then(result => console.log(result));
                 });
-                searchForm.classList.add('got-items');
+                main.classList.add('got-items');
             }
         })
         .catch(err => console.log(err));
--- src/js/show-photos.js
+++ src/js/show-photos.js
@@ -1,10 +1,11 @@
 import './show-photos.scss';
+
 const container = document.querySelector('.container');
 
-const showPhotos = async (src, description) => {
+const showPhotos = async (src) => {
     let imageContainer = document.createElement('div');
         imageContainer.classList.add('pic');
-        imageContainer.style.backgroundImage = `url('${src}')`;
+        imageContainer.style.backgroundImage = `url(${src})`;
         container.append(imageContainer);
 };
 
